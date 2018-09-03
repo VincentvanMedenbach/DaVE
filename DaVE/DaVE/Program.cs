bcce11b45f18e8fdee8b62ed7f0af57860947026
Unknown <vincent1906@live.nl>
2018-09-03 11:57:24 +0200

    Revert "Revert "Replaced Game1 With DaVE!""
    This reverts commit 16fc5f345dd17c5c30683b6b849825b883f44709.

--- DaVE/DaVE/Program.cs
+++ DaVE/DaVE/Program.cs
@@ -13,7 +13,7 @@ namespace DaVE
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new DaVE())
                 game.Run();
         }
     }
