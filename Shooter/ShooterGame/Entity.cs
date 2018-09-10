ff815d73d23dc4920b0fff1ee5d71c7ee73c5074
Unknown <vincent1906@live.nl>
2018-09-10 13:07:17 +0200

    Another Prototype test
    Test of class seperation

---
+++ Shooter/ShooterGame/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterGame
+{
+    public abstract class Entity
+    {
+        protected Texture2D image;
+
+        protected Color color = Color.White;
+
+        public Vector2 Position, Velocity;
+        public float Orientation; 
+        public float Radius = 20;
+        public bool isExpired;
+
+        public Vector2 Size
+        {
+            get
+            {
+                return image == null ? Vector2.Zero : new Vector2(image.Width, image.Height);
+            }
+        }
+
+        public abstract void Update();
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, Position, null, color, Orientation, Size / 2f, 1f, 0, 0);
+        }
+
+    }
+}
---
+++ Shooter/ShooterGame/EntityManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterGame
+{
+    static class EntityManager
+    {
+        static List<Entity> entities = new List<Entity>();
+        private static bool isUpdating; 
+        static List<Entity> addedEntities = new List<Entity>();
+
+        public static int Count { get { return entities.Count; } }
+
+        public static void Add(Entity entity)
+        {
+            if (!isUpdating)
+            {
+                entities.Add(entity);
+            }
+            else
+            {
+                addedEntities.Add(entity);
+            }
+        }
+
+        public static void Update()
+        {
+            isUpdating = true;
+
+            foreach (var entity in entities)
+                entity.Update();
+            isUpdating = false;
+
+            foreach (var entity in addedEntities)
+            {
+                entities.Add(entity);
+            }
+            addedEntities.Clear();
+            //Remove dead
+            entities = entities.Where(x => !x.isExpired).ToList();
+
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(var entity in entities)
+            {
+                entity.Draw(spriteBatch);
+            }
+        }
+    }
+}
