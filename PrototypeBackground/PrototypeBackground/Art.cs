76e8b4fca209ab149d24fdf2c48b58809936761e
Unknown <vincent1906@live.nl>
2018-09-11 13:34:17 +0200

    Prototype Background

---
+++ PrototypeBackground/PrototypeBackground/Art.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PrototypeBackground
+{
+    static class Art
+    {
+        public static Texture2D Background { get; private set; }
+       
+
+        public static void Load(ContentManager content)
+        {
+            Background = content.Load<Texture2D>("background");
+        
+        }
+    }
+}
