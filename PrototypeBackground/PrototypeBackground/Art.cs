298bc23c47d245c9c240217355d2505ff19ed17c
Unknown <vincent1906@live.nl>
2018-09-12 09:01:05 +0200

    Moveable player
    Added a moveable player

--- PrototypeBackground/PrototypeBackground/Art.cs
+++ PrototypeBackground/PrototypeBackground/Art.cs
@@ -11,12 +11,14 @@ namespace PrototypeBackground
     static class Art
     {
         public static Texture2D Background { get; private set; }
-       
+        public static Texture2D Player { get; private set; }
+
 
         public static void Load(ContentManager content)
         {
             Background = content.Load<Texture2D>("background");
-        
+            Player = content.Load<Texture2D>("Player");
+
         }
     }
 }
