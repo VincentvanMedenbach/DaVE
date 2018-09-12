62e1b14762127ef6f84ea400fcd01bc6b016e315
Unknown <vincent1906@live.nl>
2018-09-12 09:01:05 +0200

    Moveable player
    Added a moveable player

--- PrototypeBackground/PrototypeBackground/Game1.cs
+++ PrototypeBackground/PrototypeBackground/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.ComponentModel;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,13 +10,23 @@ namespace PrototypeBackground
     /// </summary>
     public class Game1 : Game
     {
+        
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
+        private Vector2 Position;
+        private Vector2 FrameSize;
+        public static Game1 Instance { get; private set; }
+        public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+        public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+
         public Game1()
-        {
+        { 
             graphics = new GraphicsDeviceManager(this);
+            Instance = this;
             Content.RootDirectory = "Content";
+            FrameSize.X = 200;
+            FrameSize.Y = 200;
+          
         }
 
         /// <summary>
@@ -37,6 +48,8 @@ namespace PrototypeBackground
         /// </summary>
         protected override void LoadContent()
         {
+            Position.X = (Viewport.Height / 2);
+            Position.Y = (Viewport.Width / 2);
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Art.Load(Content);
@@ -59,8 +72,18 @@ namespace PrototypeBackground
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            var state = Keyboard.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            if (state.IsKeyDown(Keys.Right))
+                Position.X++;
+            if (state.IsKeyDown(Keys.Left))
+                Position.X--;
+            if (state.IsKeyDown(Keys.Up))
+                Position.Y--;
+            if (state.IsKeyDown(Keys.Down))
+                Position.Y++;
 
             // TODO: Add your update logic here
 
@@ -75,8 +98,10 @@ namespace PrototypeBackground
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-
             spriteBatch.Draw(Art.Background, new Rectangle(0, 0, 800, 480), Color.White);
+
+            spriteBatch.Draw(Art.Player,
+                new Rectangle((int)Position.X, (int)Position.Y, (int)FrameSize.X, (int)FrameSize.Y), Color.White);
 
             spriteBatch.End();
             // TODO: Add your drawing code here
